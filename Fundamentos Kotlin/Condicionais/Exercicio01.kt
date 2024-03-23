b2254fc3752027ad37d8df838ba9537cf3087e41
Karinne Angelo <kakaangelo25@gmail.com>
2024-03-22 21:36:54 -0300

    Adicionando exercícios de prática

---
+++ Fundamentos Kotlin/Condicionais/Exercicio01.kt
@@ -0,0 +1,15 @@
+// if e else
+fun main() {
+    val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+
+}
