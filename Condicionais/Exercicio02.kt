f78c380bf99202931190fb91534d815deb8ab907
Karinne Angelo <kakaangelo25@gmail.com>
2024-03-22 21:48:15 -0300

    Exercícios de prática

---
+++ Condicionais/Exercicio02.kt
@@ -0,0 +1,11 @@
+// utilização do "when"
+fun main() {
+    val trafficLightColor = "Black"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
